7f7ac321615da66d86deb370f5b206f4e74e659b
Bityta <bituckihs@gmail.com>
2024-02-24 20:21:26 +0300

    code refactoring

--- src/main/java/ru/app/tasktrackerservicediscovery/TaskTrackerServiceDiscoveryApplication.java
+++ src/main/java/ru/app/tasktrackerservicediscovery/TaskTrackerServiceDiscoveryApplication.java
@@ -11,6 +11,5 @@ public class TaskTrackerServiceDiscoveryApplication {
     public static void main(String[] args) {
         SpringApplication.run(TaskTrackerServiceDiscoveryApplication.class, args);
     }
-    //geteway mb
 
 }
