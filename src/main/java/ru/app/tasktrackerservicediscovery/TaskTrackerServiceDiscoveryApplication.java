42a1bf14d69c27992df90537f3b4403b7b7a97f6
Bityta <bituckihs@gmail.com>
2024-02-26 03:14:11 +0300

    added documentation

--- src/main/java/ru/app/tasktrackerservicediscovery/TaskTrackerServiceDiscoveryApplication.java
+++ src/main/java/ru/app/tasktrackerservicediscovery/TaskTrackerServiceDiscoveryApplication.java
@@ -4,12 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Main class to start the Task Tracker Service Discovery application.
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class TaskTrackerServiceDiscoveryApplication {
 
+    /**
+     * Main method to start the Task Tracker Service Discovery application.
+     *
+     * @param args Command line arguments.
+     */
     public static void main(String[] args) {
         SpringApplication.run(TaskTrackerServiceDiscoveryApplication.class, args);
     }
-
 }
